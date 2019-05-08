6a855579c65e17b62242fbf8eb5038f30494a2b2
freewind <nowindlee@gmail.com>
2019-05-08 15:23:24 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-JavaScript Jasmine Demo
-=======================
+JavaScript Jasmine Run Node Code Demo
+=====================================
 
-在命令行中运行`Jasmine`跑测试
+才发现Jasmine是可以运行node的模块的。
+
+以前以为它只能在浏览器中运行。
 
 ```
 npm install
--- spec/hello-spec.js
+++ spec/hello-spec.js
@@ -1,7 +1,11 @@
-var hello = require('../src/hello');
+const hello = require('../src/hello');
+const fs = require('fs');
+const path = require('path');
 
 describe("A suite", function () {
   it("contains spec with an expectation", function () {
+    const content = fs.readFileSync(path.resolve(__dirname, 'support/jasmine.json'), 'utf-8')
+    console.log(content);
     expect(hello()).toBe("Hello,world!");
   });
 });
